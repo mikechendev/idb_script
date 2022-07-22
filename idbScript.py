0c78ff8a3ce759017b919692af9c6e6379e3e732
Michael Chen <michael.chen.1120@gmail.com>
2022-07-22 19:07:17 -0400

    Add chromedriver path

--- idbScript.py
+++ idbScript.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
 import webbrowser
 
-driver = webdriver.Chrome()
+#Download chromedriver from https://chromedriver.chromium.org/downloads
+driver = webdriver.Chrome("../chromedriver")
 driver.get("https://www.interview-db.com/")
-gbutton = driver.find_element_by_id("fa fa-github")
-gbutton.click()
-button = driver.find_element_by_id("sc-dnqmqq eZdSMY sc-jnlKLf kSIKvw")
-button.click()
-button2 = driver.find_element_by_id("sc-dnqmqq eKQKYW sc-jnlKLf kdusnr")
-button2.click()
+# gbutton = driver.find_element_by_id("fa fa-github")
+# gbutton.click()
+# button = driver.find_element_by_id("sc-dnqmqq eZdSMY sc-jnlKLf kSIKvw")
+# button.click()
+# button2 = driver.find_element_by_id("sc-dnqmqq eKQKYW sc-jnlKLf kdusnr")
+# button2.click()
