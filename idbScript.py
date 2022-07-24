03345052ace26b2a211af254509ebd9e9fcc40e5
Michael Chen <michael.chen.1120@gmail.com>
2022-07-24 16:56:26 -0400

    Add schedule to script

--- idbScript.py
+++ idbScript.py
@@ -3,26 +3,45 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 import webbrowser
+import schedule  # pip install schedule
+import time
 
 # Download chromedriver from https://chromedriver.chromium.org/downloads
-driver = webdriver.Chrome("../chromedriver")
-driver.get("https://www.interview-db.com/")
-link = driver.find_element("link text", "Student Sign in with Github")
-link.click()
 
-wait = WebDriverWait(driver, 10)
-wait.until(EC.element_to_be_clickable((By.NAME, "login"))).send_keys(
-    ""
-)  # enter your github username
-wait.until(EC.element_to_be_clickable((By.NAME, "password"))).send_keys(
-    ""
-)  # enter your github password
-gh = wait.until(EC.element_to_be_clickable((By.NAME, "commit"))).click()
 
-wait.until(EC.element_to_be_clickable)((By.XPATH, "//*[@id='react-tabs-2']")).click()
-wait.until(EC.element_to_be_clickable)(
-    (By.ID, "sc-dnqmqq eZdSMY sc-jnlKLf kSIKvw")
-).click()
-wait.until(EC.element_to_be_clickable)(
-    (By.ID, "sc-dnqmqq eKQKYW sc-jnlKLf kdusnr")
-).click()
+def idbScript():
+    driver = webdriver.Chrome("../chromedriver")
+    driver.get("https://www.interview-db.com/")
+    link = driver.find_element("link text", "Student Sign in with Github")
+    link.click()
+
+    wait = WebDriverWait(driver, 10)
+    wait.until(EC.element_to_be_clickable((By.NAME, "login"))).send_keys(
+        ""
+    )  # enter your github username
+    wait.until(EC.element_to_be_clickable((By.NAME, "password"))).send_keys(
+        ""
+    )  # enter your github password
+    wait.until(EC.element_to_be_clickable((By.NAME, "commit"))).click()
+
+    time.sleep(10)
+    # wait.until(EC.presence_of_element_located)((By.XPATH, "//*[@id='react-tabs-2']")).click()
+    wait.until(EC.element_to_be_clickable)(
+        (By.XPATH, "//*[@id='react-tabs-2']")
+    ).click()
+    wait.until(EC.element_to_be_clickable)(
+        (By.ID, "sc-dnqmqq eZdSMY sc-jnlKLf kSIKvw")
+    ).click()
+    wait.until(EC.element_to_be_clickable)(
+        (By.ID, "sc-dnqmqq eKQKYW sc-jnlKLf kdusnr")
+    ).click()
+
+
+schedule.every().day.at("08:50").do(idbScript)
+schedule.every().day.at("17:20").do(idbScript)
+
+while True:
+    schedule.run_pending()
+    time.sleep(60)
+
+# run with nohup python3 idbScript.py &
