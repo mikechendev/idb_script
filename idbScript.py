bcfd0bc9464ca1f167d371e9d524543772ed5c30
Michael Chen <michael.chen.1120@gmail.com>
2022-07-24 16:05:23 -0400

    Add Github login

--- idbScript.py
+++ idbScript.py
@@ -1,12 +1,28 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 import webbrowser
 
-#Download chromedriver from https://chromedriver.chromium.org/downloads
+# Download chromedriver from https://chromedriver.chromium.org/downloads
 driver = webdriver.Chrome("../chromedriver")
 driver.get("https://www.interview-db.com/")
-# gbutton = driver.find_element_by_id("fa fa-github")
-# gbutton.click()
-# button = driver.find_element_by_id("sc-dnqmqq eZdSMY sc-jnlKLf kSIKvw")
-# button.click()
-# button2 = driver.find_element_by_id("sc-dnqmqq eKQKYW sc-jnlKLf kdusnr")
-# button2.click()
+link = driver.find_element("link text", "Student Sign in with Github")
+link.click()
+
+wait = WebDriverWait(driver, 10)
+wait.until(EC.element_to_be_clickable((By.NAME, "login"))).send_keys(
+    ""
+)  # enter your github username
+wait.until(EC.element_to_be_clickable((By.NAME, "password"))).send_keys(
+    ""
+)  # enter your github password
+gh = wait.until(EC.element_to_be_clickable((By.NAME, "commit"))).click()
+
+wait.until(EC.element_to_be_clickable)((By.XPATH, "//*[@id='react-tabs-2']")).click()
+wait.until(EC.element_to_be_clickable)(
+    (By.ID, "sc-dnqmqq eZdSMY sc-jnlKLf kSIKvw")
+).click()
+wait.until(EC.element_to_be_clickable)(
+    (By.ID, "sc-dnqmqq eKQKYW sc-jnlKLf kdusnr")
+).click()
